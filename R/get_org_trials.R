fa52a2efdae6c229af545edabfd0f1b4c3cfb188
Maia Salholz-Hillel <mash2015@gmail.com>
2022-02-21 21:55:31 +0100

    Add functions to get trials by organization

---
+++ R/get_org_trials.R
@@ -0,0 +1,105 @@
+#' Get trials "led" by specified organization
+#'
+#' Trials "led" by organization if organization appears in: overall officials, sponsors (lead only), responsible parties ("principal_investigator", "study_director", "unspecified_official", "sub_investigator", "study_chair")
+#' Based on IntoValue (https://github.com/quest-bih/IntoValue2/blob/master/code/1_sample_generation/Create_CTgov_sample.R#L87)
+#'
+#' @param org Character. Regular expression for organization.
+#' @param user Character. AACT username.
+#' @param password Character. Default is NULL. If not provided, password will be searched for in \code{keyring} under the \code{aact} service with the provided \code{username}. If no password found, user will be interactively asked and input will be stored in keyring.
+  #' @param overwrite Logical. If \code{org} already queried, should it be requeried? Defaults to `FALSE`.
+#' @param query Character. Query `INFO` for \code{loggit}. Defaults to "Organization: {org}".
+#' @param ignore_case Boolean. Whether to ignore case for \code{org}. Defaults to TRUE.
+#'
+#' @return Dataframe with trials with organization as lead and "leaderhip" columns
+#'
+#' @export
+
+
+# TODO: currently, org doesn't work
+# TODO: currently, not writing to disk and not using overwrite, but could add
+
+get_org_trials <- function(org,
+                           user, password = NULL,
+                           overwrite = FALSE,
+                           query = glue::glue("Organization: {org}"),
+                           ignore_case = TRUE){
+  # Prepare log
+  LOGFILE <- here::here("queries.log")
+  loggit::set_logfile(LOGFILE)
+
+  # Inform user about tables to be queried
+  rlang::inform(glue::glue("Querying AACT for organization: {org}"))
+
+  con <- connect_aact(user, password = password)
+
+  sponsors <-
+    query_org_in_tbl(org, "sponsors", columns = c("lead_or_collaborator", "name"),
+                     con = con,
+                     ignore_case = ignore_case) %>%
+
+    # Trials are limited to lead sponsor
+    dplyr::rename(lead_sponsor = name) %>%
+    dplyr::select(nct_id, lead_sponsor)
+
+  officials <-
+    query_org_in_tbl(org, "overall_officials", columns = c("affiliation", "role"),
+                     con = con,
+                     ignore_case = ignore_case) %>%
+
+    # Some trials have multiple officials of same type and affiliation, so remove duplicates
+    dplyr::distinct() %>%
+
+    # Some trials have multiple officials of same type and different affiliation, so collapse and distinct again
+    dplyr::group_by(nct_id, role) %>%
+    dplyr::mutate(affiliation = stringr::str_c(affiliation, collapse = "; ")) %>%
+    dplyr::ungroup() %>%
+    dplyr::distinct() %>%
+
+    # Some trials have unspecified officials
+    dplyr::mutate(role = tidyr::replace_na(role, "unspecified_official")) %>%
+
+    tidyr::pivot_wider(nct_id, names_from = role, values_from = affiliation) %>%
+
+    janitor::clean_names()
+
+  responsible_parties <-
+    query_org_in_tbl(org, "responsible_parties", columns = c("affiliation", "organization"),
+                     con = con,
+                     ignore_case = ignore_case) %>%
+
+    # Check that each study has only affiliation OR organization, and then merge and rename to responsible party
+    assertr::assert_rows(assertr::num_row_NAs, assertr::in_set(1), c(affiliation, organization)) %>%
+    dplyr::mutate(affiliation = dplyr::coalesce(affiliation, organization), .keep = "unused") %>%
+    dplyr::rename(responsible_party = affiliation) %>%
+    dplyr::select(-table)
+  # tidyr::pivot_wider(nct_id, names_from = table, values_from = affiliation)
+
+  # Disconnect aact database
+  RPostgreSQL::dbDisconnect(con)
+
+  # Log query date
+  loggit::loggit("INFO", query)
+
+  org_trials <-
+    dplyr::full_join(sponsors, officials, by = "nct_id") %>%
+    dplyr::full_join(responsible_parties, by = "nct_id")
+
+  org_trials
+}
+
+# # Additional tables could be checked for organization, e.g., "results_contacts" and "central_contacts"
+#
+# # Some trials have organization email but not affiliation
+# result_contacts_email_only <-
+#   query_org_in_tbl("result_contacts", columns = c("organization", "email")) %>%
+#   filter(stringr::str_detect(organization, "(?i)stanford", negate = TRUE))
+#
+# # Limit results contacts based on organization (not email)
+# result_contacts <-
+#   query_org_in_tbl("result_contacts", columns = "organization", con) %>%
+#   rename(results_contact = organization) %>%
+#   select(-table)
+#
+# # There are also central contacts, but individual not organization
+# central_contacts <-
+#   query_org_in_tbl("central_contacts", columns = c("contact_type", "email", "name"))
---
+++ R/query_org_in_tbl.R
@@ -0,0 +1,33 @@
+#' Get trials which have organization in specified columns/table
+#'
+#' AACT documentation available in the [database schema](https://aact.ctti-clinicaltrials.org/schema) and [data dictionary](https://aact.ctti-clinicaltrials.org/data_dictionary)
+#'
+#' @param org Character. Regular expression for organization.
+#' @param table Character. AACT table.
+#' @param columns Character. Vector of column names from AACT table. `nct_id` is always returned and should not be included in this. Organization will be searched in these columns
+#' @param con Connection to AACT database as returned by \code{dbConnect}.
+#' @param ignore_case Boolean. Whether to ignore case for \code{org}. Defaults to TRUE.
+#' @return Dataframe with organization in specified columns from \code{table}
+
+# TODO: organization currently hardcoded and should be passed in as variable but not working. issue open on so: https://stackoverflow.com/questions/71208648/use-variable-with-regex-in-stringstr-detect-in-dbplyr-sql-query
+
+query_org_in_tbl <- function(org, table, columns, con, ignore_case = TRUE){
+  query <-
+    dplyr::tbl(con, table) %>%
+    # dplyr::filter(nct_id %in% ct_trns) %>%
+    dplyr::select(nct_id, dplyr::all_of(columns)) %>%
+
+    # Limit "sponsors" to "lead"
+    {if (table == "sponsors") dplyr::filter(., lead_or_collaborator == "lead") else .} %>%
+
+    dplyr::filter(dplyr::if_any(dplyr::everything(), ~stringr::str_detect(., "(?i)stanford"))) #%>%  #this works, but need to specify miami, i.e., can't use variable
+  # dplyr::filter(dplyr::if_any(dplyr::everything(), ~stringr::str_detect(., organization_regex))) %>% # I would like to use this
+  # dplyr::distinct(nct_id)
+
+  # dplyr::show_query(query)
+
+  out <- dplyr::collect(query)
+
+  out %>%
+    dplyr::mutate(table = table)
+}
